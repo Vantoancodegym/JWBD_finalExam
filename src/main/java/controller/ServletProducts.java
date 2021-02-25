df0f3a0ca6772a51bbbc9057e1b85d9f4f3d1dd9
vantoanftu93 <73921998+vantoanftu93@users.noreply.github.com>
2021-02-25 10:01:03 +0700

    create list.jsp

--- src/main/java/controller/ServletProducts.java
+++ src/main/java/controller/ServletProducts.java
@@ -1,5 +1,7 @@
 package controller;
 
+import service.ProductService;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -8,12 +10,25 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 @WebServlet(name = "ServletProducts", urlPatterns = "/products")
+
 public class ServletProducts extends HttpServlet {
+    ProductService productService=new ProductService();
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String action=request.getParameter("action");
+        if (action==null) action="";
+        switch (action){
+            default:
+                showList(request,response);
+                break;
+        }
+
+    }
+
+    private void showList(HttpServletRequest request, HttpServletResponse response) {
+    }
+
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
     }
-
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
-    }
+    
 }
